22223405ecc72e6e5f422953d161a52a3d380825
Floyden <floydenmonteiro@gmail.com>
2023-08-20 18:28:17 +0530

    JSX

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  );
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//     href: 'https://google.com',
+//     target: '_blank',
+//   },
+//   children: 'Click me to vist google',
+// };
+
+const anotherUser = 'Floyden'
+
+const reactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_black' },
+  'click me to visit google',
+  anotherUser
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(reactElement);
